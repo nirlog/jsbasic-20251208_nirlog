cf12ef93903239b0ef88d74f0763cc38532d35e2
Aleksandr Lebedev <nirlog@yandex.ru>
2025-12-16 10:58:55 +0300

    1-module/2-task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  let status = false;
+  if (typeof name == "string" && name.length >= 4 && !name.includes(" ")) {
+    status = true;
+  }
+  return status;
 }
 
 function sayHello() {
