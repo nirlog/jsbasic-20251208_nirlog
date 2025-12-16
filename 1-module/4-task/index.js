99cc8ae89b8f53f0f2e975782bb1f2dde78e28ca
Aleksandr Lebedev <nirlog@yandex.ru>
2025-12-16 11:38:07 +0300

    1-module full

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  let status = false;
+  if (str) {
+    str = str.toLowerCase();
+    if (str.includes('1xbet') || str.includes('xxx')) {
+      status = true;
+    }
+  }
+  return status;
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    let newStr = str.slice(0, maxlength - 1);
+    newStr = newStr + "…";
+    str = newStr;
+  }
+  return str;
 }
